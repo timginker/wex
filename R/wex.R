6b6b9826e20dd3af2da47bec53d10b7019477e88
Tim Ginker <timginker@gmail.com>
2025-01-02 09:41:35 +0200

    wex version 1

---
+++ R/wex.R
@@ -0,0 +1,108 @@
+#' Exact observation weights for the Kalman filter and smoother.
+#'
+#' The function computes exact observation weights for the Kalman filter and smoother,
+#' as described in Koopman and Harvey (2003). 'wex' was built upon the existing 'FKF' package.
+#'
+#' @param Tt An \code{array} giving the factor of the transition equation (see \bold{Details}).
+#' @param Zt An \code{array} giving the factor of the measurement equation (see \bold{Details}).
+#' @param HHt An \code{array} giving the variance of the innovations of the transition equation (see \bold{Details}).
+#' @param GGt An \code{array} giving the variance of the disturbances of the measurement equation (see \bold{Details}).
+#' @param yt An \eqn{n \times d}{n * d} matrix, where d is the dimension and n is the number of observations. \code{matrix} containing the observations. “NA”-values are allowed (see \bold{Details}).
+#' @param t An observation index for which the weights are returned.
+#'
+#' @import FKF
+#'
+#' @returns weight matrices for filtering and smoothing.
+#'
+#'
+#' @references Koopman, S. J., & Harvey, A. (2003). Computing observation weights for
+#'  signal extraction and filtering. \emph{Journal of Economic Dynamics and Control}, \bold{27}(7), 1317-1333.
+#'
+#'
+#'
+#' @export
+#'
+#' @details
+#'
+#' \strong{State space form}
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+wex<-function(Tt,
+              Zt,
+              HHt,
+              GGt,
+              yt,
+              t){
+
+  if(is.null(dim(yt))){
+    dim_y=1
+    n_y=length(y)
+  }else{
+    dim_y=dim(yt)[1]
+    n_y=dim(yt)[2]
+  }
+
+  # empty weights container for filtering
+  Wt<-array(matrix(0,nrow=dim(Tt)[1],
+                  ncol=dim_y),
+           dim=c(dim(Tt)[1],
+                 dim_y,
+                 n_y))
+
+  # empty weights container for smoothing
+  WtT<-array(matrix(0,nrow=dim(Tt)[1],
+                   ncol=dim_y),
+            dim=c(dim(Tt)[1],
+                  dim_y,
+                  n_y))
+
+  # computing observation weights for a given period
+
+  # loop over dimensions of yt
+  for (col in 1:dim_y) {
+    # loop over time
+    for (s in n_y:1){
+
+      data1<-matrix(0,
+                    nrow=n_y,
+                    ncol=dim_y)
+
+      data1[s,col]<-1
+      # restoring NAs
+      data1[is.na(yt)]<-NA
+
+      # Kalman Filter
+      kfw<-FKF::fkf(rep(0,dim(Tt)[1]),
+              diag(10^6,dim(Tt)[1]),
+              rep(0,dim(Tt)[1]),
+              rep(0,dim_y),
+              Tt,
+              Zt,
+              HHt,
+              GGt,
+              yt=t(data1))
+      # Kalman Smoother
+      kfws<-FKF::fks(kfw)
+
+
+      # Storing results
+      WtT[,col,s]=kfws$ahatt[,t]
+      Wt[,col,s]=kfw$att[,t]
+
+    }
+
+
+  }
+
+  return(list(Wt=Wt,
+              WtT=WtT))
+
+
+}
