2c9763466504139b3dc48f8418050531c8da8ca3
Tim Ginker <timginker@gmail.com>
2025-01-02 12:59:28 +0200

    added data on indicators

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' Sample data with 10 economic indicators
+#'
+#' A dataset containing 10 monthly economic indicators. All variables are log-differenced, if neccesary, to achieve stationarity.
+#'
+#' @format A data frame with 263 rows and 11 variables:
+#'
+#' \describe{
+#'   \item{date}{Dates}
+#'   \item{total_production}{Industrial production in Israel - total}
+#'   \item{retail_revenue}{ }
+#'   \item{services_revenue}{}
+#'   \item{employment}{}
+#'   \item{export_services}{}
+#'   \item{building_starts}{}
+#'   \item{import_consumer_goods}{}
+#'   \item{import_production_inputs}{}
+#'   \item{export_goods}{ }
+#'   \item{job_openings}{}
+#' }
+#' @source Public data
+"indicators"
