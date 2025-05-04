1c6ef105161c477a7cf025c6860ba46d933319cf
Tim Ginker <timginker@gmail.com>
2025-05-04 10:05:53 +0300

    data description

--- R/data.R
+++ R/data.R
@@ -1,21 +1,21 @@
 #' Sample data with 10 economic indicators
 #'
-#' A dataset containing 10 monthly economic indicators. All variables are log-differenced, if neccesary, to achieve stationarity.
+#' A dataset containing 10 monthly economic indicators. All variables are log-differenced, if necessary, to achieve stationarity.
 #'
 #' @format A data frame with 263 rows and 11 variables:
 #'
 #' \describe{
 #'   \item{date}{Dates}
 #'   \item{total_production}{Industrial production in Israel - total}
-#'   \item{retail_revenue}{ }
-#'   \item{services_revenue}{}
-#'   \item{employment}{}
-#'   \item{export_services}{}
-#'   \item{building_starts}{}
-#'   \item{import_consumer_goods}{}
-#'   \item{import_production_inputs}{}
-#'   \item{export_goods}{ }
-#'   \item{job_openings}{}
+#'   \item{retail_revenue}{Revenue in Trade}
+#'   \item{services_revenue}{Revenue in Services}
+#'   \item{employment}{Employment excluding absent workers}
+#'   \item{export_services}{Exports of services}
+#'   \item{building_starts}{Building starts}
+#'   \item{import_consumer_goods}{Imports of consumer goods}
+#'   \item{import_production_inputs}{Imports of production inputs}
+#'   \item{export_goods}{Exports of goods}
+#'   \item{job_openings}{Job openings}
 #' }
-#' @source Public data
+#' @source Public data from various sources
 "indicators"
